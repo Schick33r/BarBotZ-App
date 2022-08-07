99c1ddfdef1656f4bbd2676cb10f0722cdb53f06
Marcel Zimmermann <onkelz.marcel@googlemail.com>
2022-08-07 20:02:42 +0200

    Android App hinzugefuegt

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:barbotzapp/screens/login_page.dart';
 import 'screens/change_bottles.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
